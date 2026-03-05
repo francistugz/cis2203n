e8ba93c51c51e328e845999f549261aebb97aeb8
Francis Tugonon <18400139@usc.edu.ph>
2026-03-05 21:25:37 +0800

    mainactivity logic

--- app/src/main/java/com/example/hellocampus/MainActivity.java
+++ app/src/main/java/com/example/hellocampus/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.hellocampus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("ID: 18400139");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        Button myButton = findViewById(R.id.btnChangeName);
+        final TextView myText = findViewById(R.id.tvDepartment);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myText.setText("Dr. Angie Ceniza-Canillo");
+            }
+        });
+
+        Button btn = findViewById(R.id.btn_back);
+        btn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
         });
     }
 }
+
