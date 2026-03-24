5449a1dcbf5c073beeb80a17737af14220269ac6
Francis Tugonon <18400139@usc.edu.ph>
2026-03-24 11:40:55 +0800

    Layout for Calculator with theme button

---
+++ app/src/main/java/com/example/mockmidterms/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.mockmidterms;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView ResultText;
+    double firstNum = 0;
+    double secondNum =0;
+    String operator ="";
+    boolean isOperatorClicked = false;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ResultText = findViewById(R.id.ResultText);
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+}
