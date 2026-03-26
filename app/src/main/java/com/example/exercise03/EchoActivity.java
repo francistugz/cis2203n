8e820acdf7dd42bfc8008c3230db8add666085fc
Francis Tugonon <18400139@usc.edu.ph>
2026-03-26 14:25:28 +0800

    Did the logic in echoactivity

--- app/src/main/java/com/example/exercise03/EchoActivity.java
+++ app/src/main/java/com/example/exercise03/EchoActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.content.Intent;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class EchoActivity extends AppCompatActivity {
+
+    public static final String TAG = "EchoActivity";
+    TextView txtMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,16 @@ public class EchoActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_echo);
 
+        Log.d(TAG, "onCreate called");
+
+        txtMessage = findViewById(R.id.txtMessage);
+
+        // Get message from Intent
+        String message = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        Log.d(TAG, "Received message: " + message);
+
+        txtMessage.setText(message);
 
             ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.echo), (v, insets) -> {
                 Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
